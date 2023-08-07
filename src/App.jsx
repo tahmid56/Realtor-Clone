95ab9469300321555dc28b9b1fa4993706c8a43e
Tahmid Akter <tahmidakter56@gmail.com>
2023-08-07 19:01:00 +0600

    pages and routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react';
-import { app } from './utils/firebaseConfig';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 
 function App() {
-    let auth = getAuth();
-    const [userCred, setUserCred] = useState({ name: '', password: '' });
-    const handleSubmit = () => {
-        createUserWithEmailAndPassword(auth, userCred.name, userCred.password)
-            .then((res) => console.log(res.user))
-            .catch((error) => alert(err.message));
-    };
     return (
         <>
-            <div>
-                <div></div>
-                <div className="font-semibold text-3xl">Hello</div>
-                <div></div>
-            </div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/offers" element={<Offers/>}/>
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                </Routes>
+            </Router>
         </>
     );
 }
